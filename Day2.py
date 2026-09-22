cb6f44e784ede0eadc7b897429048c4f33c9d4de
Rakshitha-Muppidi <rakshithamuppidi03@gmail.com>
2026-09-22 23:01:55 +0530

    Operators

---
+++ Day2.py
@@ -0,0 +1,45 @@
+#operators
+# 1.Arithmetical operators
+a=10
+b=3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+# 2.Assignment operators
+a=10
+b=5
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# 3.Logical operators
+# and
+age=25
+print(age > 18 and age < 30)
+
+# or
+age=15
+print(age > 18 or age > 30)
+
+# not
+x=True
+print(not(x))
+
+# 4.Assignment Operators
+x=10
+x+=5
+print(x)
+
+x-=3
+print(x)
+
+x*=2
+print(x)
