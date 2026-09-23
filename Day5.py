de5b9b1194b3eac86fa033ec257ad364404f6161
rakshithamuppidi <rakshithamuppidi03@gmail.com>
2026-09-23 11:58:06 +0530

    string opreations & methods

---
+++ Day5.py
@@ -0,0 +1,14 @@
+#Built-in-functions
+#String-Sequence of characters written inside the quotes- which is immutable data type-we can do slicing
+#Sring operations
+# 1.String concatenation
+
+str1="python"
+str2="programming"
+print(str1 + " " + str2)
+
+# 2.string repetition
+a="abc"
+print(a * 3)
+
+# 3.
