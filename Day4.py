22f8a90284632805e54985cb986c6324f670e01d
rakshithamuppidi <rakshithamuppidi03@gmail.com>
2026-09-23 11:38:55 +0530

    Datatypes

---
+++ Day4.py
@@ -0,0 +1,55 @@
+# Datatypes-conversion of one data type to another data type
+#Numbers
+# 1.Integer
+age=21
+print("Integer:",age)
+print(type(age))
+
+# 2.Float
+price=99.50
+print("Float:", price)
+print(type(price))
+
+#3.Complex
+number=3+4j
+print("complex:", number)
+print(type(number))
+
+#Sequence Datatypes
+# 1.String
+name="rakshitha"
+print(name)
+print(type(name))
+# 2.Tuple
+nums=(1,2,3)
+print(nums)
+print(type(nums))
+# 3.List
+l=[10,20,30]
+print(l)
+print(type(l))
+
+# Boolean Datatype 
+is_student=True
+print("Boolean:", is_student)
+print(type(is_student))
+
+#Mapping Datatype
+# 1.Dictionaries
+student={"name": "Rakshitha", "age": 21, "course": "Python"}
+print(student)
+print(type(student))
+#sets
+colors={"Red", "Green", "Blue"}
+print(colors)
+print(type(colors))
+
+# Frozenset
+unique_nums=frozenset([1,2,3,4])
+print(unique_nums)
+print(type(unique_nums))
+
+#None type
+result=None
+print(result)
+print(type(result))
